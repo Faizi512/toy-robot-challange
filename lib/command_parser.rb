f87c75e75e7eefc4206bc516eb668f5e041a104b
Faizan Haider <hfaizan53@gmail.com>
2026-01-20 18:49:16 +1100

    addressing the PR comments:

--- lib/command_parser.rb
+++ lib/command_parser.rb
@@ -2,33 +2,36 @@ require_relative 'direction'
 
 module CommandParser
 
-    VALID_COMMANDS = %w[MOVE LEFT RIGHT REPORT PLACE].freeze
+    class << self
 
-    def self.parse(command)
-        cmd, args = command.strip.upcase.split(' ', 2)
-        return invalid_command unless VALID_COMMANDS.include?(cmd)
+        def parse(command)
+            cmd, args = command.strip.upcase.split(' ', 2)
+            return invalid_command unless VALID_COMMANDS.include?(cmd)
 
-        case cmd
-        when 'PLACE' then parse_place(args)
-        else
-            {command: cmd.downcase.to_sym}
+            case cmd
+            when 'PLACE' then parse_place(args)
+            else
+                {command: cmd.downcase.to_sym}
+            end
+        end
+
+        private
+
+        VALID_COMMANDS = %w[MOVE LEFT RIGHT REPORT PLACE].freeze
+
+        def parse_place(args)
+            return invalid_command if args.nil?
+
+            x, y, direction = args.split(',').map(&:strip)
+            return invalid_command unless x && y && direction
+
+            return invalid_command unless Direction.valid?(direction.downcase.to_sym)
+
+            {command: :place, x: x.to_i, y: y.to_i, direction: direction.downcase.to_sym}
+        end
+
+        def invalid_command
+            { command: :invalid, hint: "Valid commands: #{VALID_COMMANDS.join(', ')}" }
         end
     end
-
-    def self.parse_place(args)
-        return invalid_command if args.nil?
-
-        x, y, direction = args.split(',').map(&:strip)
-        return invalid_command unless x && y && direction
-
-        return invalid_command unless Direction.valid?(direction.downcase.to_sym)
-
-        {command: :place, x: x.to_i, y: y.to_i, direction: direction.downcase.to_sym}
-    end
-    private_class_method :parse_place
-
-    def self.invalid_command
-        { command: :invalid, hint: "Valid commands: #{VALID_COMMANDS.join(', ')}" }
-    end
-    private_class_method :invalid_command
 end
